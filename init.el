ab3956e6d2878c9fa504c2dc1cb8e4bbba20669f
Alexander Derevianko <alexander0derevianko@gmail.com>
2024-05-22 14:03:17 +0000

    Emacs from scratch #2

--- init.el
+++ init.el
@@ -37,7 +37,7 @@
 	 ("C-k" . ivy-previous-line)
 	 ("C-l" . ivy-done)
 	 ("C-d" . ivy-switch-biffer-kill)
-	 :map ivy-revese-i-search-line
+	 :map ivy-reverse-i-search-map
 	 ("C-k" . ivy-previous-line)
 	 ("C-d" . ivy-reverse-i-search-kill))
   :config
@@ -45,3 +45,54 @@
 
 ;; Escape to cancel
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Line numbers
+(column-number-mode)
+(global-display-line-numbers-mode t)
+;;Disable for some modes
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;; Rainbow delimiters
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; Which-key
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history))
+  :config
+  (setq ivy-initial-inputs-alist nil)) ;; Don't start seraches with ^
+
+;; Ivy rich 
+(use-package ivy-rich
+  :init (ivy-rich-mode 1))
+
+;; Helpful
+(use-package helpful
+  :ensure t
+  :custom
+  (counsel-describe-fuction-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+;; Doom themes
+(use-package doom-themes
+  :ensure t
+  :config
+  (load-theme 'doom-gruvbox t))
